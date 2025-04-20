787bef1ecf4e11106dc15c26526e6fc69f668395
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2025-04-20 21:32:18 +0200

    implement dependency injection for DioClient, PexelsApi, VideoRepository, and VideoCubit

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,5 +1,27 @@
 import 'package:get_it/get_it.dart';
 
-final getIt = GetIt.instance;
+import '../../features/profile/data/repo/video_repo.dart';
+import '../../features/profile/logic/cubit/video/videos_cubit.dart';
+import '../networking/api_services.dart';
+import '../networking/dio_factory.dart';
 
-Future<void> setupGetIt() async {}
+final getIt = GetIt.instance;
+Future<void> setupGetIt() async {
+  // Dio Client
+  getIt.registerLazySingleton<DioClient>(() => DioClient());
+
+  // API Layer
+  getIt.registerLazySingleton<PexelsApi>(
+    () => PexelsApi(getIt<DioClient>().client),
+  );
+
+  // Repository
+  getIt.registerLazySingleton<VideoRepository>(
+    () => VideoRepository(getIt<PexelsApi>()),
+  );
+
+  // Cubit
+  getIt.registerFactory<VideoCubit>(
+    () => VideoCubit(getIt<VideoRepository>()),
+  );
+}
