a0621d335ddbcd09648ec7729405c054e6caa428
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2025-04-20 21:31:50 +0200

    add PexelsApi service and custom logger interceptor for API requests

--- lib/core/networking/dio_factory.dart
+++ lib/core/networking/dio_factory.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
-import 'package:flutter/foundation.dart';
 
 class DioClient {
   final Dio _dio = Dio();
