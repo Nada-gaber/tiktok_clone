442a80b84bc3a72774b27b3f3f2900d3982d1e39
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2025-04-20 19:36:17 +0200

    load environment variables from .tiktok.env in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:tiktok_clone/core/routing/app_router.dart';
 import 'package:tiktok_clone/core/themes/colors.dart';
 import 'package:tiktok_clone/tiktok.dart';
@@ -8,6 +9,9 @@ import 'core/di/dependency_injection.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".tiktok.env");
+
   setupGetIt();
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     systemNavigationBarColor: AppColors.backgroundDarkBlue,
