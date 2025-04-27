17b099ba7db0a98f19bdb6b2655b832157d8f69b
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2025-04-27 22:04:12 +0200

    feat: add login and register routes, and refactor navigation icons to use AppAssets

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,6 +1,8 @@
 class Routes {
   static const String home = '/home';
   static const String navBar = '/navbar';
-
+static const String login = '/login';
+  static const String register = '/register';
+  
 
 }
