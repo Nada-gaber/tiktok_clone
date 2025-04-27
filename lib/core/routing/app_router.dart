3d106d10858408dea34696d19b48bd581d8babf2
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2025-04-27 22:04:12 +0200

    feat: add login and register routes, and refactor navigation icons to use AppAssets

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/core/routing/routes.dart';
+import 'package:tiktok_clone/features/auth/presentation/screens/login_screen.dart';
+import '../../features/auth/presentation/screens/register_screen.dart';
 import '../../features/home/persentation/screens/home_screen.dart';
 import '../../features/home/persentation/screens/nav_screen.dart';
 
@@ -14,6 +16,14 @@ class AppRouter {
         case Routes.navBar:
         return MaterialPageRoute(
           builder: (context) => const NavScreen(),
+        );
+        case Routes.login:
+        return MaterialPageRoute(
+          builder: (context) => const LoginScreen(),
+        );
+        case Routes.register:
+        return MaterialPageRoute(
+          builder: (context) => const RegisterScreen(),
         );
     }
     return null;
