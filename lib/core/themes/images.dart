74353de9a8ea8d96b3ddf232fc224b16ba8d117c
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2025-05-03 00:30:19 +0200

    feat: add color property to regular font style and organize asset icons in AppAssets

--- lib/core/themes/images.dart
+++ lib/core/themes/images.dart
@@ -1,7 +1,15 @@
 class AppAssets {
+
+  // Navigation Bar Icons
   static const String addButton = 'assets/icons/navbar/add.svg';
   static const String homeIcon = 'assets/icons/navbar/home.svg';
   static const String searchIcon = 'assets/icons/navbar/search.svg';
   static const String inboxIcon = 'assets/icons/navbar/inbox.svg';
   static const String profileIcon = 'assets/icons/navbar/profile.svg';
+
+//Reels Icons
+  static const String heartIcon = 'assets/icons/reels/Heart Icon.svg';
+  static const String commentIcon = 'assets/icons/reels/Message Icon.svg';
+  static const String shareIcon = 'assets/icons/reels/Share Icon.svg';
+
 }
