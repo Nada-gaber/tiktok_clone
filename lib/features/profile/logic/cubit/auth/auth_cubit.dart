6affbaa7fa8a0ebff658236cbfba8023dfc3aa7d
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2025-05-24 17:21:13 +0200

    refactor: streamline auth state checks and remove redundant import

--- lib/features/profile/logic/cubit/auth/auth_cubit.dart
+++ lib/features/profile/logic/cubit/auth/auth_cubit.dart
@@ -1,10 +1,9 @@
+import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../data/models/user_model.dart';
 import 'auth_state.dart';
-import 'dart:developer';
-
 
 class AuthCubit extends Cubit<AuthState> {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
@@ -30,17 +29,33 @@ class AuthCubit extends Cubit<AuthState> {
         if (doc.exists) {
           final userModel = UserModel.fromJson(doc.data()!);
           log('Emitting authenticated state for user: ${userModel.email}');
+          if (isClosed) {
+            log('AuthCubit is closed, cannot emit state');
+            return;
+          }
           emit(AuthState.authenticated(user: userModel));
         } else {
           log('No Firestore profile found for user: ${user.uid}');
+          if (isClosed) {
+            log('AuthCubit is closed, cannot emit state');
+            return;
+          }
           emit(const AuthState.unauthenticated());
         }
       } else {
         log('No user signed in');
+        if (isClosed) {
+          log('AuthCubit is closed, cannot emit state');
+          return;
+        }
         emit(const AuthState.unauthenticated());
       }
     } catch (e) {
       log('Error checking auth: $e');
+      if (isClosed) {
+        log('AuthCubit is closed, cannot emit state');
+        return;
+      }
       emit(const AuthState.unauthenticated());
     }
   }
@@ -49,6 +64,10 @@ class AuthCubit extends Cubit<AuthState> {
     try {
       log('Signing out');
       await _firebaseAuth.signOut();
+      if (isClosed) {
+        log('AuthCubit is closed, cannot emit state');
+        return;
+      }
       emit(const AuthState.unauthenticated());
     } catch (e) {
       log('Error signing out: $e');
