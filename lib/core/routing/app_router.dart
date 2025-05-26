9fbd2bb38bb23c22a79beedc0d08c80a72d87a2c
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2025-05-26 22:28:46 +0200

    feat: add onboarding route to app router

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tiktok_clone/core/routing/routes.dart';
+import 'package:tiktok_clone/features/onboarding/screens/onboarding_screens.dart';
 import 'package:tiktok_clone/features/profile/logic/cubit/auth/auth_cubit.dart';
 import '../../features/auth/presentation/screens/register_screen.dart';
 import '../../features/home/persentation/screens/home_screen.dart';
@@ -24,6 +25,11 @@ class AppRouter {
       case Routes.register:
         return MaterialPageRoute(
           builder: (context) => const RegisterSheet(),
+        );
+
+      case Routes.onBoarding:
+        return MaterialPageRoute(
+          builder: (context) => const OnboardingScreens(),
         );
     }
     return null;
