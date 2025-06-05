2c4a08a20e0b1d45c55d93e0ae6eedcf391299a0
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2025-06-05 21:21:22 +0200

    feat: Integrate Hive for local data storage and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:tiktok_clone/core/routing/app_router.dart';
 import 'package:tiktok_clone/tiktok.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'features/posts/data/model/post_model.dart';
 import 'firebase_options.dart';
 import 'core/di/dependency_injection.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load(fileName: ".tiktok.env");
-  
+  await Hive.initFlutter();
+  Hive.registerAdapter(PostAdapter());
+  await Hive.openBox<Post>('posts');
   setupGetIt();
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     systemNavigationBarColor: Colors.white,
