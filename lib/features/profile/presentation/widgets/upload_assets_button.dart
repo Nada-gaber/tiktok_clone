59cd674b020dfea4e3158e1f6b6c2645dbc854ea
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2025-06-05 15:00:23 +0200

    fix: Add button dimensions to CustomUploadButton for consistent styling

--- lib/features/profile/presentation/widgets/upload_assets_button.dart
+++ lib/features/profile/presentation/widgets/upload_assets_button.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import '../../../../core/themes/app_sizes.dart';
 import '../../../../core/widgets/shared_button.dart';
 import '../../../home/persentation/helpers/post_helper.dart';
 import '../../../posts/data/model/post_model.dart';
-
 
 typedef OnPostCreated = void Function(Post post);
 
@@ -10,7 +10,6 @@ class CustomUploadButton extends StatelessWidget {
   final OnPostCreated onPostCreated;
 
   const CustomUploadButton({super.key, required this.onPostCreated});
-
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +19,8 @@ class CustomUploadButton extends StatelessWidget {
         onPostCreated: onPostCreated,
       ),
       buttonText: 'Create Post',
+      minWidth: AppSizes.buttonWidthHalf(context),
+      buttonHeight: AppSizes.buttonHeight(context),
     );
   }
 }
